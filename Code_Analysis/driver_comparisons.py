c2e32b80b0e87faa82d73fb20d63df0f2f47f0f5
Cam Stevens <86251531+CamStevens15@users.noreply.github.com>
2022-06-29 18:12:27 -0700

    add in module to easy load race track heat map for specific driver

--- Code_Analysis/driver_comparisons.py
+++ Code_Analysis/driver_comparisons.py
@@ -1,4 +1,9 @@
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+from matplotlib import cm
+from matplotlib.pyplot import figure
+from matplotlib.collections import LineCollection
+
 import numpy as np
 import pandas as pd
 
@@ -7,13 +12,83 @@ from fastf1 import utils
 from fastf1.core import Laps
 from fastf1 import plotting
 
-def get_driver_from_session(session):
+from timple.timedelta import strftimedelta
+
+
+def main():
+    ''' This calls on the fastf1 api and creates a session based on year,
+     grand prix, and session(Quali, race, sprint) wanted '''
+    # Setup plotting
+    plotting.setup_mpl()
+
+    # Enable the cache
+    ff1.Cache.enable_cache('/Users/cameronstevens/Documents/Coding/F1_2022_race_analytics/cache') 
+
+    # Get rid of some pandas warnings that are not relevant for us at the moment
+    pd.options.mode.chained_assignment = None 
+
+    # we only want support for timedelta plotting in this example
+    ff1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme=None, misc_mpl_mods=False)
+
+    #This loads the session for the Grand prix session of interest
+
+    
+
+   
+
+def get_driver_from_session(year, grand_prix, driver1, session):
     '''This function gets the respective driver from the session of intererst
     from the fast f1 API.'''
+    session = ff1.get_session(year, grand_prix, session)
+    session.load() # This is new with Fastf1 v.2.2
+
+    lap = session.laps.pick_driver(driver1).pick_fastest()
+    colormap = plt.cm.plasma
+    # Get telemetry data
+    x = lap.telemetry['X']              # values for x-axis
+    y = lap.telemetry['Y']              # values for y-axis
+    color = lap.telemetry['Speed']      # value to base color gradient on
+
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # We create a plot with title and adjust some setting to make it look good.
+    fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
+    fig.suptitle(f'{grand_prix} {year} - {driver1} - Speed', size=24, y=0.97)
+
+    # Adjust margins and turn of axis
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
+    ax.axis('off')
+
+
+    # After this, we plot the data itself.
+    # Create background track line
+    ax.plot(lap.telemetry['X'], lap.telemetry['Y'], color='black', linestyle='-', linewidth=16, zorder=0)
+
+    # Create a continuous norm to map from data points to colors
+    norm = plt.Normalize(color.min(), color.max())
+    lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
+
+    # Set the values used for colormapping
+    lc.set_array(color)
+
+    # Merge all line segments together
+    line = ax.add_collection(lc)
+
+
+    # Finally, we create a color bar as a legend.
+    cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
+    normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
+    legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
+
+
+    # Show the plot
+    plt.show()
 
 
 
-def driver_speed_change(driver1):
+
+def plot_driver_speed_change(driver1):
 
     
     colormap = plt.cm.plasma
@@ -61,24 +136,3 @@ def driver_speed_change(driver1):
     # Show the plot
     plt.show()
 
-
-
-def main():
-    # Setup plotting
-    plotting.setup_mpl()
-
-    # Enable the cache
-    ff1.Cache.enable_cache('/Users/cameronstevens/Documents/Coding/F1_2022_race_analytics/cache') 
-
-    # Get rid of some pandas warnings that are not relevant for us at the moment
-    pd.options.mode.chained_assignment = None 
-
-    # we only want support for timedelta plotting in this example
-    ff1.plotting.setup_mpl(mpl_timedelta_support=True, color_scheme=None, misc_mpl_mods=False)
-
-    #This loads the session for the Grand prix session of interest
-
-    year, grand_prix, session = 2022, '', ''
-
-    session = ff1.get_session(year, grand_prix, session)
-    session.load() # This is new with Fastf1 v.2.2
